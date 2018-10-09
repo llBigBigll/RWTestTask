87aef9df2e78353409607157743760ce8806a61c
llBigBigll <nikitaulyankinipad@gmail.com>
2018-10-09 20:28:58 +0300

    Ended and optimized (unoptimized  solution were commented)

--- RWTestTask/Program.cs
+++ RWTestTask/Program.cs
@@ -3,19 +3,164 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace RWTestTask
 {
+
     class Program
     {
+        static int NumOfLines(string filename)
+        {
+            using (StreamReader strm = new StreamReader(filename))
+            {
+                int i = 0;
+                while (strm.ReadLine() != null)
+                {
+                    string a = strm.ReadLine();
+                    i++;
+                }
+                return i;
+            }
+        }
+        static char[] InvalidLetters()
+        { List<char> chars = new List<char>();
+            for (char i = char.MinValue; i < char.MaxValue; i++)
+            {
+                if (!((i >= '0' && i <= '9') || (i >= 'A' && i <= 'Z') || (i >= 'a' && i < 'z')))
+                {
+                    chars.Add(i);
+                }
+            }
+            return chars.ToArray();
+        }
+        static Dictionary<string, uint> TaskWork(string str, char[] chr)
+        {
+            Dictionary<string, uint> dic = new Dictionary<string, uint>();
+            if (str != null)
+            {
+                string[] wordsarr = str.Split(chr, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in wordsarr)
+                {
+
+                    if (dic.ContainsKey(word.ToLower()))
+                    {
+                        dic[word.ToLower()] += 1;
+
+                    }
+                    else
+                    {
+                        dic.Add(word.ToLower(), 1);
+                    }
+                }
+            }
+            return dic;
+        }
+    
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            string FileName = "C:/Users/nikit/source/repos/RWTestTask/RWTestTask/WarAndPeace.txt";
+            string FileOfAnswer = "C:/Users/nikit/source/repos/RWTestTask/RWTestTask/Answer.txt";
+            int linesForOneTask = NumOfLines(FileName) /12;
+            Console.WriteLine(NumOfLines(FileName).ToString());
+            Console.WriteLine(linesForOneTask.ToString());
+
+
+            string[] parts = new string[10];
+
+            using (StreamReader filestream = new StreamReader(FileName))
+            {
+                int i = 0;
+                int counter = 0;
+
+                while (filestream.ReadLine() != null)
+                {
+
+                    string line = filestream.ReadLine();
+                    parts[i] += line;
+                    counter++;
+                    
+                    if (counter >= linesForOneTask && i<=8)
+                    {
+                        counter = 0;
+                        i++;
+                    }
+                    
+                }
+            }
+
+
+            char[] invchar = InvalidLetters();
+            Task<Dictionary<string, uint>>[] tasks = { Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[0]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[1]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[2]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[3]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[4]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[5]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[6]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[7]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[8]), invchar)),
+                Task<Dictionary<string, uint>>.Factory.StartNew(() => TaskWork((parts[9]), invchar))};
+            Task.WaitAll(tasks);
+
+            SortedDictionary<string, uint> WordsList = new SortedDictionary<string, uint>();
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+
+                foreach (KeyValuePair<string, uint> pair in tasks[i].Result)
+                {
+
+                    if (WordsList.ContainsKey(pair.Key))
+                    {
+                        WordsList[pair.Key] += pair.Value;
+                    }
+                    else
+                    {
+                        WordsList.Add(pair.Key,pair.Value);
+                    }
+                }
+            }
+
+            //using (StreamReader filestream = new StreamReader(FileName))
+            //{
+            //    while (filestream.ReadLine() != null) {
+                    
+            //        string line = filestream.ReadLine();
+            //        string[] wordsarr = line.Split(InvalidLetters(), StringSplitOptions.RemoveEmptyEntries);
+            //        foreach (string word in wordsarr)
+            //        {
+            //            if (WordsList.ContainsKey(word.ToLower()))
+            //            {
+            //                WordsList[word.ToLower()] += 1;
+                            
+            //            }
+            //            else
+            //            {
+            //                WordsList.Add(word.ToLower(), 1);
+            //            }
+            //        }               
+            //    }
+            //}
+            Console.WriteLine("Файл успешно прочтен");
+            using (StreamWriter filestream = new StreamWriter(FileOfAnswer))
+            {
+                foreach (KeyValuePair<string, uint> word in WordsList)
+                {
+                    filestream.WriteLine(word.Key + ": "+ word.Value.ToString());
+                }
+            }
+            Console.WriteLine("Ответ записан");
+
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds.ToString());
         }
     }
 }
